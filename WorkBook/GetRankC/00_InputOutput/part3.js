77bf15643b86db136d6c35c2f62ef33c028f7b9a
momo-41 <minimizu41@gmail.com>
2025-02-05 19:18:40 +0900

    constを使った標準入力を作成

---
+++ WorkBook/GetRankC/00_InputOutput/part3.js
@@ -0,0 +1,18 @@
+// ここから標準入力はvarではなくconstで定義した形で行う
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+const readline = require("readline");
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const lines = [];
+
+reader.on("line", (line) => {
+  lines.push(line);
+});
+
+reader.on("close", () => {});
